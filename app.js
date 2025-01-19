794cd5ab1363ec9b58382556c530fbe3d862be0f
Schnappo90 <alexwilliamknapp@gmail.com>
2025-01-19 20:27:20 +0000

    Updated files, added UI elements to play the game in browser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 // Global Scores
 let humanScore = 0;
 let computerScore = 0;
@@ -55,6 +56,8 @@ function playRound(humanSelection,computerChoice){
         console.log(`Player Score: ${humanScore}`)
         console.log(`Computer Score: ${computerScore}`)
     }
+
+    scores.innerText = `Your Score: ${humanScore} vs Computer Score: ${computerScore}`;
 }
 
 function finalScores() {
@@ -64,31 +67,87 @@ function finalScores() {
     humanScore = 0;
     computerScore = 0;
 }
-   
+
+
 
 function playGame() {
-    let round = 1;
+    // let round = 1;
 
-    console.log("Welcome to Rock Paper Scissors!");
-    for(let i = 1; i <= 5; i++) { 
-        console.log(`Round ${round }`);
-        const humanSelection = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        const result = playRound(humanSelection,computerChoice);
-        console.log(result);            
-        round++;
+    // console.log("Welcome to Rock Paper Scissors!");
+    // for(let i = 1; i <= 5; i++) { 
+    //     console.log(`Round ${round }`);
+    //     const humanSelection = getHumanChoice();
+    //     const computerChoice = getComputerChoice();
+    //     const result = playRound(humanSelection,computerChoice);
+    //     console.log(result);            
+    //     round++;
     
-    } if (humanScore > computerScore) {
-        console.log(`You win overall!`);
+    // } if (humanScore > computerScore) {
+    //     console.log(`You win overall!`);
       
     
-    } else if(computerScore > humanScore){
-        console.log("The Computer wins overall!");
+    // } else if(computerScore > humanScore){
+    //     console.log("The Computer wins overall!");
     
-    } else {
-        console.log("It's a draw overall!");
+    // } else {
+    //     console.log("It's a draw overall!");
     
-    } 
-    finalScores();
+    // } 
+    // finalScores();
 
     }
+
+const button_rock = document.createElement("button");
+const button_paper = document.createElement("button");
+const button_scissors = document.createElement("button");
+const button_start = document.createElement("button");
+const div = document.createElement("div");
+const results_div = document.createElement("div");
+const scores = document.createElement("p");
+
+
+div.classList.add("btn_container");
+const btn_container = document.querySelector(".btn_container");
+button_start.textContent = 'Start Game';
+button_rock.textContent = 'Rock';
+button_paper.textContent = 'Paper';
+button_scissors.textContent = 'Scissors';
+
+document.body.appendChild(div);
+document.body.appendChild(results_div);
+div.appendChild(button_start);
+
+
+button_start.addEventListener('click',() => {
+    div.appendChild(button_rock);
+    div.appendChild(button_paper);
+    div.appendChild(button_scissors);
+    results_div.appendChild(scores);
+    button_start.remove();
+
+    button_rock.addEventListener('click', () => {
+        playRound('rock',getComputerChoice());
+    })
+    
+    button_paper.addEventListener('click', () => {
+        playRound('paper',getComputerChoice());
+    })
+    
+    button_scissors.addEventListener('click', () => {
+        playRound('scissors',getComputerChoice());
+    })
+
+})
+
+
+button_rock.addEventListener('click', () => {
+    playRound('rock',getComputerChoice());
+})
+
+button_paper.addEventListener('click', () => {
+    playRound('paper',getComputerChoice());
+})
+
+button_scissors.addEventListener('click', () => {
+    playRound('scissors',getComputerChoice());
+})
